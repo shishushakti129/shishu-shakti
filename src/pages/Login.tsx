30c588748a4454b2c0774fcdd74c8d33daaf799e
Vignesh Mariappan <vigneshmv2395@gmail.com>
2025-12-27 14:48:39 +0530

    css changes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -56,7 +56,7 @@ export const Login: React.FC = () => {
               <motion.button
                 onClick={handleSignIn}
                 disabled={loading}
-                className="btn-gentle btn-primary w-full"
+                className="btn-gentle  w-full"
                 initial={{ opacity: 0, scale: 0.9 }}
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.4, delay: 0.6 }}
@@ -66,7 +66,7 @@ export const Login: React.FC = () => {
             {loading ? (
               <span className="loading loading-spinner loading-md"></span>
             ) : (
-              <>
+              <div className="flex items-center gap-2 justify-center text-black">
                 <svg
                   className="w-5 h-5 mr-2"
                   viewBox="0 0 24 24"
@@ -78,7 +78,7 @@ export const Login: React.FC = () => {
                   <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
                 </svg>
                 Continue with Google
-              </>
+              </div>
                 )}
               </motion.button>
 
